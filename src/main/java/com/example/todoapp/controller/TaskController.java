301fbb43236ff861183c0a49f6091f9d2dd4e757
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-11-11 17:13:45 +0100

    Change structure

--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -2,18 +2,18 @@ package com.example.todoapp.controller;
 
 import com.example.todoapp.model.Task;
 import com.example.todoapp.model.TaskRepository;
+import jakarta.validation.Valid;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-
 import org.slf4j.Logger;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 
-@RepositoryRestController
+@RestController
 public class TaskController {
     private final TaskRepository repository;
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
@@ -32,4 +32,28 @@ public class TaskController {
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate){
+        if(!repository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        toUpdate.setId(id);
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/tasks/{id}")
+    ResponseEntity<Task> readTask(@PathVariable int id){
+        return repository.findById(id)
+                .map(task -> ResponseEntity.ok(task))
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping("/tasks")
+    ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate){
+        Task result = repository.save(toCreate);
+        return ResponseEntity.created(URI.create("/"+ result.getId())).body(result);
+    }
+
+
 }
--- src/main/java/com/example/todoapp/model/TaskRepository.java
+++ src/main/java/com/example/todoapp/model/TaskRepository.java
@@ -1,25 +1,18 @@
 package com.example.todoapp.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
+import java.util.Optional;
 
-@RepositoryRestResource(path = "todos",collectionResourceRel = "todos")
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-    // block sending DELETE
-    @Override
-    @RestResource(exported = false)
-    void deleteById(Integer integer);
-
-    @Override
-    @RestResource(exported = false)
-    void delete(Task task);
-
-    // seperate path for sql request
-    @RestResource(path="done", rel = "done") // address: /tasks/search/done?state=true
+public interface TaskRepository { // whitelist of methods
+    List<Task> findAll();
+    Optional<Task> findById(Integer id);
+    Task save(Task entity);
+    Page<Task> findAll(Pageable page);
+    boolean existsById(Integer id);
     List<Task> findByDone(@Param("state") boolean done);
 
 }
